b17a2177274aac0ce0f16cbd86f8a148a5559345
Maurice <m.ackel@icloud.com>
2021-01-27 17:32:39 +0100

    Added better handling for non-ssl mode

--- Backend/Api Gateway/src/config/Config.ts
+++ Backend/Api Gateway/src/config/Config.ts
@@ -1,11 +1,12 @@
 import fs from 'fs'
 
-export const PORT: number = Number.parseInt(process.env.PORT || "8443");
+export const SSL_ENABLED: boolean = process.env.SSL_ENABLED === "true";
+
+export const PORT: number = Number.parseInt(process.env.PORT || SSL_ENABLED ? "8443" : "8080");
 export const HOST: string = process.env.HOST || "localhost";
 
-export const SSL_ENABLED: boolean = process.env.SSL_ENABLED === "true";
-export const TLS_KEY: string = fs.readFileSync(`${process.env.TLS_SECRETS_PATH || '/etc/secrets-volume/tls-secret'}/key`, 'utf8');
-export const TLS_CERT: string = fs.readFileSync(`${process.env.TLS_SECRETS_PATH || '/etc/secrets-volume/tls-secret'}/cert`, 'utf8');
+export const TLS_KEY: string = SSL_ENABLED ? fs.readFileSync(`${process.env.TLS_SECRETS_PATH || '/etc/secrets-volume/tls-secret'}/key`, 'utf8') : '';
+export const TLS_CERT: string = SSL_ENABLED ? fs.readFileSync(`${process.env.TLS_SECRETS_PATH || '/etc/secrets-volume/tls-secret'}/cert`, 'utf8') : '';
 
 export const TIMEOUT: number = Number.parseInt(process.env.TIMEOUT || "30000"); //30 sec
 export const CORS_ORIGIN: Array<string> = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',').map(e => e.trim()) : [];
