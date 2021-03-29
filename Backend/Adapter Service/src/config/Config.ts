f497ca67c88cdbdbad10475f482fe445b722918f
Maurice <m.ackel@icloud.com>
2021-03-29 14:52:31 +0200

    Upgraded asyncapi dependencies

--- Backend/Adapter Service/src/config/Config.ts
+++ Backend/Adapter Service/src/config/Config.ts
@@ -3,10 +3,6 @@ import fs from 'fs';
 export const PORT: number = Number.parseInt(process.env.PORT || "8080");
 export const HOST: string = process.env.HOST || "localhost";
 
-export const DB_URL: string = process.env.DB_URL || "";
-export const DB_USER: string = process.env.DB_USER || "";
-export const DB_PW: string = process.env.DB_PW || "";
-
 export const ACCESS_TOKEN_PUB: Buffer = fs.readFileSync(`${process.env.ACCESS_TOKEN_PATH || '/etc/secrets-volume/access-token'}/public-key`);
 
 export const API_KEY = process.env.API_KEY || "";
