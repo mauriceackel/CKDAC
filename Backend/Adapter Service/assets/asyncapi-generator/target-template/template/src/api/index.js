a04f6aca588c1829cf1c02048d6eb780e6ed41e1
Maurice <m.ackel@icloud.com>
2021-04-09 14:57:39 +0200

    Updated asyncapi adapter generators

--- Backend/Adapter Service/assets/asyncapi-generator/target-template/template/src/api/index.js
+++ Backend/Adapter Service/assets/asyncapi-generator/target-template/template/src/api/index.js
@@ -9,6 +9,9 @@ const errorLogger = require('./middlewares/error-logger');
 const config = require('../lib/config');
 {%- set protocol = asyncapi.server(params.server).protocol() %}
 const {{ protocol | capitalize }}Adapter = require('hermesjs-{{protocol}}');
+{%- for channelName, channel in asyncapi.channels() %}
+const {{ channelName | camelCase }} = require('./routes/{{ channelName | convertToFilename }}.js');
+{%- endfor %}
 
 app.addAdapter({{ protocol | capitalize }}Adapter, config.{% if protocol === 'ws' %}ws{% else %}broker.{{protocol}}{% endif %});
 
@@ -16,7 +19,19 @@ app.use(buffer2string);
 app.use(string2json);
 app.use(logger);
 
+// Channels
+{% for channelName, channel in asyncapi.channels() -%}
+{% if channel.hasPublish() -%}
+console.log(cyan.bold.inverse(' SUB '), gray('Subscribed to'), yellow('{{channelName}}'));
+app.use({{ channelName | camelCase }});
+{% endif -%}
+{% if channel.hasSubscribe() -%}
+console.log(yellow.bold.inverse(' PUB '), gray('Will eventually publish to'), yellow('{{channelName}}'));
+app.useOutbound({{ channelName | camelCase }});
+{% endif -%}
+{% endfor %}
 app.use(errorLogger);
+app.useOutbound(errorLogger);
 app.useOutbound(logger);
 app.useOutbound(json2string);
 
